9b31bf10d7c60ce2f348486dfb961eccf1a9e10a
wyhx21 <2247819253@qq.com>
2020-12-10 15:38:49 +0800

    update

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -3,7 +3,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const EncodingPlugin = require('webpack-encoding-plugin');
 const { VueLoaderPlugin } = require('vue-loader')
-const PUBLIC_PATH = '/h5/'
 
 module.exports = {
   entry: {
@@ -11,9 +10,7 @@ module.exports = {
   },
   output: {
     filename: 'js/[name].[contenthash].js',
-    path: path.join(__dirname, '../dist' + PUBLIC_PATH),
     assetModuleFilename: 'images/[hash][ext][query]',
-    publicPath: PUBLIC_PATH,
   },
   plugins: [
     new HtmlWebpackPlugin({
--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -1,13 +1,19 @@
-const common = require('./webpack.common.js')
+const path = require('path')
 const { merge } = require('webpack-merge')
+const common = require('./webpack.common.js')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin')
 const TerserJSPlugin = require('terser-webpack-plugin')
 const webpack = require('webpack')
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const PUBLIC_PATH = '/h5/'
 
 module.exports = merge(common, {
   mode: 'production',
+  output: {
+    path: path.join(__dirname, '../dist' + PUBLIC_PATH),
+    publicPath: PUBLIC_PATH,
+  },
   plugins: [
     new CleanWebpackPlugin(),
     new OptimizeCssAssetsWebpackPlugin(),

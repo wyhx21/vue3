587a61e20ab66dda9d1bd4f256f8d4f9e3c57487
wyhx21 <2247819253@qq.com>
2020-12-12 13:09:18 +0800

    update

--- src/component/system/Messager.vue
+++ src/component/system/Messager.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="app-messager">
     <div class="app-data-item" v-for="item of msgList" :key="item.id">
-      <span class="msg-title">{{item.title}}</span>
-      <p class="msg-content">{{item.msg}}<span class="app-link" @click="toPage(item['link'])" v-if="item['link']">点击跳转>></span></p>
+      <h4>{{item.title}}</h4>
+      <p>{{item.msg}}<span class="app-link" @click="toPage(item['link'])" v-if="item['link']">点击跳转>></span></p>
     </div>
   </div>
 </template>
@@ -12,10 +12,10 @@ export default {
     return {
       msgList: [
         {id: 1,title: '标题1', msg: '内容1',link:'/link/aaa'},
-        {id: 2,title: '标题2', msg: '内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2'},
+        {id: 2,title: '标题2', msg: '内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2容2内容2内容2内容2内容2内容2内容2内容2内容2内容2容2内容2内容2内容2'},
         {id: 3,title: '标题3', msg: '内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2'},
-        {id: 4,title: '标题4', msg: '内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2',link:'/link/aaa'},
-        {id: 5,title: '标题5', msg: '内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2'}
+        {id: 4,title: '标题4', msg: '内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2容2内容2内容2内容2内容2内容2内容2内容2内容2内容2容2内容2内容2内容2内容2',link:'/link/aaa'},
+        {id: 5,title: '标题5', msg: '内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2容2内容2内容2内容2内容2内容2'}
       ]
     }
   },
